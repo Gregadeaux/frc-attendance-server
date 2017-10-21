e5a18ca1eaf2eab60f0e7124f949136acdd0d63a
Greg Billetdeaux <greg@rokkincat.com>
2017-10-21 13:46:36 -0500

    COORSSS

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://gregadeaux.github.io");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
   next();
